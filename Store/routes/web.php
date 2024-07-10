46a5d01476709dea34aca532d8319dbec3da69a9
AMC-3\LENOVO <coceijanina@yahoo.com>
2024-07-10 13:26:37 +0300

    user controller

--- Store/routes/web.php
+++ Store/routes/web.php
@@ -1,15 +1,12 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\User\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -18,3 +15,8 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::middleware(['auth', 'userMiddleware'])->group(function()
+{
+Route::get('dashboard', [UserController::class,'index'])->name('dashboard');
+});
